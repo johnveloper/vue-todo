61bdc08f729893922b4a185213c0b29a8d5a4278
johnveloper <johnveloper@gmail.com>
2019-04-09 14:34:51 +0200

    Add item list

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 let app = new Vue({
   el: '#app',
   data: {
-    text: ''
+    text: '',
+    items: []
+  },
+  methods: {
+    addItem: function() {
+      if (this.text.length > 0) {
+        this.items.unshift({
+          id: Date.now(),
+          text: this.text,
+          done: false
+        });
+      }
+      this.clearInput(); 
+    },
+    clearInput: function() {
+      this.text = '';
+    },
+    markItemAsDone: function(id) {
+      let itemIndex = this.items.findIndex(
+        function(item) {
+          return item.id == id;
+        }
+      );
+      let item = this.items[itemIndex];
+      item.done = !item.done;
+      if (item.done) {
+        this.items.push(this.items.splice(itemIndex, 1)[0]);
+      } else {
+        this.items.unshift(this.items.splice(itemIndex, 1)[0]);
+      }
+    }
   }
 });
